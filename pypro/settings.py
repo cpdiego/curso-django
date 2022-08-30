977be5a6c20a45b4c48e65a5d59131895bbc1f3f
Diego <cpdiego91@gmail.com>
2022-08-29 21:15:22 -0300

    Instalado collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -133,7 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 COLLECTFAST_ENABLED = False
 
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
 AWS_ACCESS_KEY_ID=config('AWS_ACCESS_KEY_ID')
 
@@ -147,10 +146,11 @@ if AWS_ACCESS_KEY_ID:
     AWS_PRELOAD_METADA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
-    AWS_S3_CUSTOM_DOMAIN = None
 
     COLLECTFAST_ENABLED = True
+    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
+    AWS_S3_CUSTOM_DOMAIN = None
     AWS_DEFAULT_ACL = 'private'
 
     # Static Assets
